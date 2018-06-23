e63fa0f20bde6dc4b753543444dd1ecb5537c383
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-06-23 14:32:46 -0400

    Add Question class

---
+++ Question.java
@@ -0,0 +1,44 @@
+public class Question{
+
+
+	protected String questionText;
+	protected int pointsPossible, pointsEarned;
+	protected int difficulty;
+	protected String correctAnswer;
+	protected int answerSpace;
+
+	public String getQuestionText() {
+		return getQuestion;
+	}
+
+	public int getPointsPossible() {
+		return pointsPossible
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+
+	public String getCorrectAnswer() {
+		return String;
+	}
+
+	public int getAnswerSpace() {
+		return answerSpace;
+	}
+
+
+
+
+
+
+
+	public String toString() {
+			return this.getQuestion();
+	}
+
+
+
+
+
+}
