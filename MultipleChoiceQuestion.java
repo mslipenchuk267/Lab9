76244513622551e6e95de55e0fa2c8bf0808b081
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-06-23 14:54:59 -0400

    name change

--- MultipleChoiceQuestion.java
+++ MultipleChoiceQuestion.java
@@ -3,7 +3,15 @@ import java.util.ArrayList;
 public class MultipleChoiceQuestion extends Question {
 	List<String> possibleAnswer = new ArrayList<String>();
 
-	public void addAnswer(String answer) {
-		this.getPossibleAnswer.add(answer);
+	public void addAnswer(String userAnswer) {
+		this.getPossibleAnswer.add(userAnswer);
+	}
+
+	public void removeAnswer(String userAnswer) {
+		this.getPossibleAnswer.remove(userAnswer);
+	}
+
+	public void setPossibleAnswer(List<String> userAnswer) {
+		this.possibleAnswer = userAnswer;
 	}
 }
